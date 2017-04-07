c64d5bc733ddc30863bcb5e8d56e6b06a111eec8
tuftscs <tuftscs@tuftscsvm.tuftscsvm>
2017-04-06 22:23:19 -0400

    added index.html, style.css, added some get and post functions

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 
@@ -7,22 +6,41 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+/*app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs'); */
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.get('/cool', function(request, response) {
-  response.send(cool());
+  //response.render('pages/index');
+  response.sendFile('/index.html');
 });
 
 app.post('/submit', function(request, response) {
   response.header("Access-Control-Allow-Origin", "*");
   response.header("Access-Control-Allow-Headers", "X-Requested-With");
-  
-  response.json({"vehicles":[], "passengers":[]})
+
+  var username = request.query.username;
+  var lat = request.query.lat;
+  var lng = request.query.lng;
+
+  if (!username || !lat || !lng) {
+  	response.json({"error":"Whoops, something is wrong with your data!"});
+  }
+
+/*  if (passenger) {
+    response.json({"vehicles":[]});
+  } */
+  response.json({"passengers":[]});
+});
+
+app.get('/vehicle.json', function(request, response) {
+  var username = request.query.username;
+  if (!username) {
+  	response.json({});
+  } else {
+	response.json({"_id":"589bd30f8451126182dfbc62","username":username,"lat":10.1,"lng":10.2,"created_at":"2017-02-09T02:25:19.575Z"});
+  }
+  //vehicle = 
+  //response.json(vehicle);
 });
 
 app.listen(app.get('port'), function() {
